7c14ea5ab0ce011e43c4b7469731eb85e6188672
binaerverkehr <65082683+binaerverkehr@users.noreply.github.com>
2026-03-16 12:48:34 +0100

    Added view "counter_view"

--- counter_app/counter/views.py
+++ counter_app/counter/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def counter_view(request):
+    count = request.session.get('count', 0)
+
+    if request.method == 'POST':
+        action = request.POST.get('action')
+        if action == 'increment':
+            count += 1
+        elif action == 'decrement':
+            count -= 1
+        elif action == 'reset':
+            count = 0
+        request.session['count'] = count
+
+    return render(request, 'counter/index.html', {'count': count})
